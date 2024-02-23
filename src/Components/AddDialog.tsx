d6c2182874578200ffc22cf8c4a1796670d47a59
vhardelin <valentin.hardelin@orange.fr>
2024-02-23 15:46:59 +0100

    Change button add on modal

--- src/Components/AddDialog.tsx
+++ src/Components/AddDialog.tsx
@@ -37,7 +37,7 @@ const AddDialog = ({setShowModal, handleNameChange, handleDescriptionChange, han
                                 <button type="button"
                                         onClick={() => handleSubmit()}
                                         className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700 mt-4">
-                                    Ajouter un équipage
+                                    Ajouter
                                 </button>
                                 <button type="button"
                                         onClick={() => setShowModal(false)}
